8bd1145639be27436394f10e5e8366c0a40d92a8
Erick Velasco <fervel1704@gmail.com>
2020-11-11 21:18:58 -0400

    gitignore file added

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,13 +1,15 @@
+import java.awt.event.KeyEvent
+
 fun main() {
     val saludo:String = ":: Bienvenido a Recipe Maker ::\n"
     val instruccion:String = "Selecciona la opción deseada"
     val menu:List<String> = listOf<String>("1. Hacer una receta", "2. Ver mis recetas", "3. Salir")
     var response:String?; var default_msg:Boolean = true
     salir@while(true) {
-//        if(default_msg) {
+        if(default_msg) {
             println(saludo + instruccion)
             for (i in menu) println(i)
-//        }
+        }
         response = readLine()
         when (if(response == "") 0 else response?.toInt() ?: 0) {
             1 -> {  println("Usted seleccionó: " + menu[0]);
@@ -18,6 +20,7 @@ fun main() {
             2 -> {println("Usted seleccionó: " + menu[1]); default_msg = false}
             3 -> break@salir
             else -> default_msg = true //println("$instruccion${menu.forEach{print(it)}}")
+
         }
     }
 }
@@ -32,3 +35,7 @@ fun makeRecipe(){
 fun viewRecipe(){
     print("Ver mis recetas")
 }
+
+//fun enterPressed(keyCode: Int, event: KeyEvent):Boolean{
+//    return (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP)
+//}
